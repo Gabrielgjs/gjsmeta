ab94edef91ff3f5f0413677d75d761c0a082b2c2
gjsgabriel <gjsgabriel83@gmail.com>
2022-07-12 13:46:15 -0300

    Project created

---
+++ backend/src/main/java/com/gjsgabriel/gjsmeta/GjsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.gjsgabriel.gjsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjsmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gjsgabriel/gjsmeta/GjsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjsgabriel.gjsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
