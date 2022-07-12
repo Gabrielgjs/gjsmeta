e7954081ad2fc3acca9a597503a6e8bbab4b2d3d
gjsgabriel <gjsgabriel83@gmail.com>
2022-07-12 15:39:12 -0300

    other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="gjsmeta-logo-container">
+                <img src={logo} alt="GJSmeta" />
+                <h1>GJSMeta</h1>
+                <p>Desenvolvido por 
+
+                    <a target="_blank" href="https://github.com/Gabrielgjs">@Gabrielgjs</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+
+export default Header
---
+++ frontend/src/components/SalesCard/index.tsx
@@ -0,0 +1,83 @@
+import NotificationButton from '../NotificationButton'
+import './styles.css'
+
+function SalesCard() {
+  return (
+    <div className="gjsmeta-card">
+      <h2 className="gjsmeta-sales-title">Vendas</h2>
+      <div>
+        <div className="gjsmeta-form-control-container">
+          <input className="gjsmeta-form-control" type="text" />
+        </div>
+        <div className="gjsmeta-form-control-container">
+          <input className="gjsmeta-form-control" type="text" />
+        </div>
+      </div>
+
+      <div>
+        <table className="gjsmeta-sales-table">
+          <thead>
+            <tr>
+              <th className="show992">ID</th>
+              <th className="show576">Data</th>
+              <th>Vendedor</th>
+              <th className="show992">Visitas</th>
+              <th className="show992">Vendas</th>
+              <th>Total</th>
+              <th>Notificar</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">8</td>
+              <td className="show992">7</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="gjsmeta-red-btn-container" >
+                  <NotificationButton />
+                </div>
+
+              </td>
+            </tr>
+            <tr>
+              <td className="show992">#342</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">12</td>
+              <td className="show992">10</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="gjsmeta-red-btn-container" >
+                  <NotificationButton />
+                </div>
+
+              </td>
+            </tr>
+            <tr>
+              <td className="show992">#343</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">18</td>
+              <td className="show992">13</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="gjsmeta-red-btn-container" >
+                  <NotificationButton />
+                </div>
+
+              </td>
+            </tr>
+          </tbody>
+
+        </table>
+      </div>
+
+    </div>
+  )
+}
+
+
+export default SalesCard
