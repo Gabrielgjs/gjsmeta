8ca8aa20691faa72b28b5e078cf95ce6fd50b021
gjsgabriel <gjsgabriel83@gmail.com>
2022-07-21 13:45:19 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../models/sale";
+import { BASE_URL } from "../../utils/request";
 import NotificationButton from '../NotificationButton';
 import './styles.css';
 
@@ -14,12 +16,14 @@ function SalesCard() {
 
   const [minDate, setMinDate] = useState(min);
   const [maxDate, setMaxDate] = useState(max);
+  const [sales, setSales] = useState<Sale[]>([]);
+
 
   useEffect(() => {
-    axios.get("http://localhost:8080/sales")
-    .then(response => {
-      console.log(response.data);
-    })
+    axios.get(`${BASE_URL}/sales`)
+      .then(response => {
+        setSales(response.data.content);
+      })
   }, []);
 
   return (
@@ -58,48 +62,24 @@ function SalesCard() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">8</td>
-              <td className="show992">7</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="gjsmeta-red-btn-container" >
-                  <NotificationButton />
-                </div>
+            {sales.map(sale => {
+              return (
+                <tr key={sale.id}>
+                  <td className="show992">{sale.id}</td>
+                  <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                  <td>{sale.sellerName}</td>
+                  <td className="show992">{sale.visited}</td>
+                  <td className="show992">{sale.deals}</td>
+                  <td>R$ {sale.amount.toFixed(2)}</td>
+                  <td>
+                    <div className="gjsmeta-red-btn-container" >
+                      <NotificationButton />
+                    </div>
 
-              </td>
-            </tr>
-            <tr>
-              <td className="show992">#342</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">12</td>
-              <td className="show992">10</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="gjsmeta-red-btn-container" >
-                  <NotificationButton />
-                </div>
-
-              </td>
-            </tr>
-            <tr>
-              <td className="show992">#343</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">18</td>
-              <td className="show992">13</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="gjsmeta-red-btn-container" >
-                  <NotificationButton />
-                </div>
-
-              </td>
-            </tr>
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
 
         </table>
